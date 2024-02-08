ed0d7b603dd23612d2b05fe24a0fea00dd28bc65
Skeby <103804946+Skeby@users.noreply.github.com>
2024-02-08 20:46:10 +0100

    feat: design rate card component

--- oneport-special-rates/src/App.jsx
+++ oneport-special-rates/src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import CarrierButton from "./components/CarrierButton";
+import RateCard from "./components/RateCard";
 
 const App = () => {
   const carriers = ["COSCO", "HMM", "MAERSK", "EVERGREEN", "OOCL", "YANG MING"];
+  const rates = ["$1,885", "$1,885", "$1,885", "$1,885", "$1,885", "$1,885"];
   return (
     <div className="max-w-[1200px] mx-auto relative px-5 py-10 text-4xl col-span-1">
       <div className="relative min-h-[843px]">
@@ -20,6 +22,13 @@ const App = () => {
               </CarrierButton>
             ))}
           </div>
+        </div>
+
+        {/* Card Rates */}
+        <div className="card-rates pt-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+          {rates.map((rate, i) => (
+            <RateCard key={i} rate={rate} />
+          ))}
         </div>
       </div>
     </div>
---
+++ oneport-special-rates/src/components/RateCard.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import link from "../assets/link.svg";
+
+const RateCard = () => {
+  return (
+    <div className="p-5 rounded-[10px] cursor-pointer border-[2px] border-solid border-[#E5E7EB] hover:border-[#139C33]">
+      <div className="flex items-center justify-between">
+        <p className="text-sm text-black font-medium">COSCO</p>
+        <div className="black-text-3 flex text-sm font-normal items-center gap-x-2">
+          <p>CNSZX</p>
+          <span>
+            <img src={link} alt />
+          </span>
+          <p>GHTEM</p>
+        </div>
+      </div>
+      <div className="mt-3 mb-6 flex items-center justify-between">
+        <p className="text-xl font-normal text-[#004800]">$1,885</p>
+      </div>
+      <div className="top-divider text-sm pt-6 grid grid-cols-3">
+        <div>
+          <p className="grey-text font-light mb-1.5">Sailing Date</p>
+          <p className="black-text-3">N/A</p>
+        </div>
+        <div>
+          <p className="grey-text font-light mb-1.5">Transit Time</p>
+          <p className="black-text-3">N/A</p>
+        </div>
+        <div>
+          <p className="grey-text font-light mb-1.5">Free Days</p>
+          <p className="black-text-3">N/A</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RateCard;
