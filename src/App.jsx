02924380296cc4b76de6502fa840626fd5be5cee
Skeby <103804946+Skeby@users.noreply.github.com>
2024-02-09 14:37:24 +0100

    feat: implement no rates display when no rates are available

--- src/App.jsx
+++ src/App.jsx
@@ -105,20 +105,24 @@ const App = () => {
           </div>
         </div>
 
-        {/* Card Rates */}
-        <div className="card-rates pt-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
-          {displayedRates.map((rate, index) => (
-            <RateCard
-              key={index}
-              carrier_name={rate.carrier_name}
-              origin_port_code={rate.origin_port_code}
-              destination_port_code={rate.destination_port_code}
-              sailing_date={rate.sailing_date}
-              transit_time={rate.transit_time}
-              free_days={rate.detention_days + rate.demurrage_days}
-            />
-          ))}
-        </div>
+        {/* Rates Cards */}
+        {totalRateCount === 0 ? (
+          <p className="mt-4">No Rates To Display</p>
+        ) : (
+          <div className="card-rates pt-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+            {displayedRates.map((rate, index) => (
+              <RateCard
+                key={index}
+                carrier_name={rate.carrier_name}
+                origin_port_code={rate.origin_port_code}
+                destination_port_code={rate.destination_port_code}
+                sailing_date={rate.sailing_date}
+                transit_time={rate.transit_time}
+                free_days={rate.detention_days + rate.demurrage_days}
+              />
+            ))}
+          </div>
+        )}
 
         {/* Additional Rates */}
         {totalRateCount > defaultRateCount && (
